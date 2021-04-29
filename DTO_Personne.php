491c7dc77b9c1fe67f1332b3d51971a63502296b
johan <johan.maurice@hotmail.fr>
2021-04-29 23:15:02 +0200

    debut creation catalogue / oeuvre

--- DTO_Personne.php
+++ DTO_Personne.php
@@ -6,5 +6,51 @@ Class DTOPersonne{
 	private $nom;
 	private $prenom;
 
-	public function construct
+	public function __construct($idP,$nom,$prenom){
+		$this->idPersonne=$idP;
+		$this->nom=$nom;
+		$this->prenom=$prenom;
+	}
+
+	public function __get($attribut){
+
+		switch ($attribut) {
+			case 'id':
+				return $this->idPersonne;
+				break;
+			
+			case 'nom':
+				retrun $this->nom;
+				break;
+
+			case 'prenom':
+				return $this->prenom;
+				break;
+
+			default:
+				return "erreur"
+				break;
+		}
+
+	}
+
+	public function __set($attribut, $valeur){
+
+		switch ($attribut) {
+			case 'id':
+				$this->idPersonne=$valeur;
+				break;
+			
+			case 'nom':
+				$this->nom=$valeur;
+				break;
+
+			case 'prenom':
+				$this->prenom=$valeur;
+				break;
+
+			default:
+				return "erreur"
+				break;
+		}
 }
