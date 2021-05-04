e2933b10d6c73163c1c839fdcf721032dcd1923d
kemiliabensassi <75848091+kemiliabensassi@users.noreply.github.com>
2021-05-04 14:24:27 +0200

    Update recherche.php

--- Vue/recherche.php
+++ Vue/recherche.php
@@ -1,4 +1,4 @@
 <form class="recherche" action="index.php" method="get">
-	<label>Recherche<input type="text" name="recherche"></label>
-	<button name=btnRecherche>Rechercher</button>
+	<label>Recherche<input type="text" name="recherche" class="bouton_recherche"  placeholder="Tapez le nom de l'oeuvre que vous cherchez"></label>
+	<button name=btnRecherche class="valider">Rechercher</button>
 </form>
